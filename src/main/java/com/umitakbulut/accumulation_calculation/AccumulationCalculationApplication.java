a695434cb28ffa185c5269a1dc7858f4f09e2319
deneme <com.deneme>
2025-06-29 20:46:26 +0300

    Initial commit

---
+++ src/main/java/com/umitakbulut/accumulation_calculation/AccumulationCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.umitakbulut.accumulation_calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccumulationCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccumulationCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umitakbulut/accumulation_calculation/AccumulationCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umitakbulut.accumulation_calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccumulationCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
